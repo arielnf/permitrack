96b2277aef40165575ea47024b4740ee143abe58
Ariel <anune.1281@gmail.com>
2024-11-25 01:45:43 -0300

    Agrega manejo de errores en el hook useAuth

--- src/pages/confirm.js
+++ src/pages/confirm.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import { supabase } from "../lib/supabaseClient";
 
 export default function ConfirmPage() {
