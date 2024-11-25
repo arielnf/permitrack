3f1e08c60b896d378486d4ec9966cb48eee10d21
Ariel <anune.1281@gmail.com>
2024-11-25 11:49:36 -0300

    cambio supackient

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Obtiene las variables de entorno
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+// Se asegúrate de que las variables no estén vacías
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL y/o clave anónima no configuradas correctamente');
+}
+
+console.log('supabaseUrl:', supabaseUrl);  // Verifica que la URL esté cargada correctamente
+console.log('supabaseAnonKey:', supabaseAnonKey);  // Verifica que la clave esté cargada correctamente
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
